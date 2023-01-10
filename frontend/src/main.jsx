8756bd0a59c8c4b384d4b90a3fe1c6d007fd5c79
Kevin Zhang <zhang.kevin.hua@gmail.com>
2023-01-10 12:59:39 -0600

    redux installed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Provider } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
+import configureStore from './store'
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>,
 )
