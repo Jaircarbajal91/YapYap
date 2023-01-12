c56ecbd4b90c6d03c439859290c18699ebfd8fe3
Kevin Zhang <zhang.kevin.hua@gmail.com>
2023-01-12 01:56:33 -0600

    frontend working yayyy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
-      
+        <h1>Hi!!!!</h1>
     </div>
   )
 }
