5ddfddc70eae101c681aa91b90477754ae4f6b38
Kevin Zhang <zhang.kevin.hua@gmail.com>
2023-01-12 01:26:19 -0600

    models generated

---
+++ backend/generate_migrations.sh
@@ -0,0 +1,5 @@
+npx sequelize-cli migration:generate --name user
+npx sequelize-cli migration:generate --name image
+npx sequelize-cli migration:generate --name message
+npx sequelize-cli migration:generate --name chat
+npx sequelize-cli migration:generate --name chat-member
