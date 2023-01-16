a55d252ac885dfb19dfb935cad52907f245393e9
Jair Carbajal <jair.carbajal91@gmail.com>
2023-01-16 14:43:05 -0800

    trying configure vite proxy server to send requests to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from 'react'
+import { Route, Switch } from 'react-router-dom';
 import { useSelector } from 'react-redux'
+import LoginForm from './auth/LoginForm'
 import './App.css'
 
 function App() {
   const sessionUser = useSelector(session => session.user)
   return (
     <div className="App">
-        <h1>Hi!!!!</h1>
+        <Switch>
+          <Route path='/' exact={true}>
+            <LoginForm sessionUser={sessionUser} />
+          </Route>
+        </Switch>
     </div>
   )
 }
--- frontend/src/auth/LoginForm.jsx
+++ frontend/src/auth/LoginForm.jsx
@@ -1,5 +1,78 @@
-const LoginForm = () => {
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { login } from '../store/session';
+import './LoginForm.css';
 
+
+const LoginForm = ({ sessionUser }) => {
+  const dispatch = useDispatch();
+  const session = useSelector(state => state.session);
+  const [credential, setCredential] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  const onLogin = async (e) => {
+    e.preventDefault();
+    const data = await dispatch(login(credential, password));
+    console.log(data)
+    if (data) {
+      setErrors(data);
+    } else {
+      return (
+        <Redirect to="/" />
+      )
+    }
+  };
+
+  const updateCredential = (e) => {
+    setCredential(e.target.value);
+  };
+
+  const updatePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  if (sessionUser) return (
+    <Redirect to="/" />
+  );
+
+  return (
+    <div className="login-form-container">
+      <form className="login-form" onSubmit={onLogin}>
+        <div className="login-form__header">
+          <h1 className="login-form__header__title">Log In</h1>
+        </div>
+        <div className="login-form__body">
+          <div className="login-form__body__input">
+            <label className="login-form__body__input__label">Email/Username: </label>
+            <input
+              className="login-form__body__input__input"
+              type="text"
+              name="email"
+              placeholder="Email/Username"
+              value={credential}
+              onChange={updateCredential}
+            />
+          </div>
+          <div className="login-form__body__input">
+            <label className="login-form__body__input__label">Password: </label>
+            <input
+              className="login-form__body__input__input"
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={updatePassword}
+            />
+          </div>
+        </div>
+        <div className="login-form__footer">
+          <button className="login-form__footer__button" type="submit">Log In</button>
+        </div>
+      </form>
+    </div>
+  )
 }
 
-
+export default LoginForm;
