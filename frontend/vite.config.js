760bf0adbe3d2bc47b2d627fb97ea47b4f689940
Jair Carbajal <jair.carbajal91@gmail.com>
2023-01-16 15:53:50 -0800

    still figuring out proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// define config to proxy requests to backend
+const proxy = {
+  '/': {
+    target: 'http://localhost:8000/',
+    changeOrigin: true,
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '5173': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    },
-  }
+  server: { proxy } // define proxy in server config
 })
