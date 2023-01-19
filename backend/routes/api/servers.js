f51fd407ba580daadbb29a22d7dcf9991d5a11d0
Kevin Zhang <zhang.kevin.hua@gmail.com>
2023-01-19 07:21:51 -0600

    added checkAuth function to servers.js router

--- backend/routes/api/servers.js
+++ backend/routes/api/servers.js
@@ -3,21 +3,21 @@ const { Server, ChatMember } = require("../../db/models");
 
 const router = express.Router();
 
+const checkAuth = (req, res, next) => !req.user ? next(new Error("Please log in or register to access this information.")) : next();
+
 // Get all servers for current user
-router.get("/", async (req, res) => {
-    console.log(req.user)
-    // console.log(currentUserId);
-    // const servers = await Server.findAll({
-    //     include: [
-    //         {
-    //             model: ChatMember,
-    //             where: { user_id: currentUserId }
-    //         }
-    //     ]
-    // });
-    // console.log(res.json(servers))
-    // return res.json(servers);
-    return res.json({ message: 'hello' })
+router.get("/", checkAuth, async (req, res) => {
+    const user_id = req.user.id;
+    const servers = await Server.findAll({
+        include: [
+            {
+                model: ChatMember,
+                where: { user_id }
+            }
+        ]
+    });
+    console.log(res.json(servers))
+    return res.json(servers);
 });
 
 
