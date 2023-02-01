f4ecafac5af7b9d6a7f9dbe9943e7ae576b98d72
Kevin Zhang <zhang.kevin.hua@gmail.com>
2023-02-01 10:23:24 -0600

    changed build.outDir to build--default was dist

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,16 @@
-import { build, defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: build,
+    outDir: './build',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
   },
   plugins: [react()],
   server: {
