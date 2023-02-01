e1de6159f6dd738a978ba36481860fe0360f1089
Jair Carbajal <jair.carbajal91@gmail.com>
2023-02-01 14:46:29 -0800

    added db to heroku

--- backend/config/database.js
+++ backend/config/database.js
@@ -10,7 +10,7 @@ module.exports = {
     typeValidation: true
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: 'HEROKU_POSTGRESQL_RED_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
     dialectOptions: {
