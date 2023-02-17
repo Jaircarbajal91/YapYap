a273a4ec9df976934f7ac326526984547f62814e
Jair Carbajal <jair.carbajal91@gmail.com>
2023-02-17 10:53:40 -0800

    4th

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    tailwindcss: {},
-    autoprefixer: {},
+  mode: 'jit',
   build: {
     outDir: './build',
   },
