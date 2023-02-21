d045e86d3df127c7c552e7f92e38d5a1136b2015
Jair Carbajal <jair.carbajal91@gmail.com>
2023-02-21 14:02:51 -0800

    starting to import differently

--- frontend/src/components/Splash/splash.jsx
+++ frontend/src/components/Splash/splash.jsx
@@ -4,6 +4,8 @@ import { Redirect, useHistory } from "react-router-dom";
 import Navbar from '../Navbar'
 import DemoButton from "../demoButton";
 import { csrfFetch } from "../../store/csrf";
+import splash2 from '../../assets/images/splash2.svg'
+import splash3 from '../../assets/images/splash3.svg'
 import './splash.css'
 
 const Splash = ({ sessionUser }) => {
@@ -13,13 +15,13 @@ const Splash = ({ sessionUser }) => {
         <div className="splash-main-container">
             <Navbar />
             <div className="splash-wrapper">
-                <img src="../../../assets/images/splash2.svg" alt="" />
+                <img src={splash2} alt="" />
                 <div className="splash-message">
                     <h1 className="">IMAGINE A PLACE...</h1>
                     <p className="">...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</p>
                     <DemoButton />
                 </div>
-                <img src="../../../assets/images/splash3.svg" alt="" />
+                <img src={splash3} alt="" />
             </div>
         </div>
     )
