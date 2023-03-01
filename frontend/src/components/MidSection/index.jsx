d59aab8cd74dc3ee304c74998bad0a2bdd3a201b
Jair Carbajal <jair.carbajal91@gmail.com>
2023-02-28 18:43:21 -0800

    fixed another import statement

--- frontend/src/components/MidSection/index.jsx
+++ frontend/src/components/MidSection/index.jsx
@@ -1,26 +1,30 @@
-import DirectMessagesList from "../DirectMessages"
-import Channels from "../Channels"
-import { useSelector, useDispatch } from "react-redux"
-import { getDirectMessages } from "../../store/DirectMessages"
-import { useEffect, useState } from "react"
+import DirectMessagesList from "../DirectMessages";
+import Channels from "../Channels";
+import { useSelector, useDispatch } from "react-redux";
+import { getDirectMessages } from "../../store/directMessages";
+import { useEffect, useState } from "react";
 
+const MidSection = ({ serverClicked }) => {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
 
-
-const MidSection = ({serverClicked}) => {
-  const dispatch = useDispatch()
-  const [isLoaded, setIsLoaded] = useState(false)
-
-  const directMessages = Object.values(useSelector(state => state.dms))
+  const directMessages = Object.values(useSelector((state) => state.dms));
 
   useEffect(() => {
-    dispatch(getDirectMessages()).then(() => setIsLoaded(true))
-  }, [dispatch])
+    dispatch(getDirectMessages()).then(() => setIsLoaded(true));
+  }, [dispatch]);
 
-  return isLoaded && (
-    <div className='relative z-0 min-w-[18em] w-[18em] max-w-[18em] py-2 px-3 min-h-screen max-h-screen overflow-auto bg-midGray'>
-      {serverClicked ? <Channels/> : <DirectMessagesList directMessages={directMessages}/>}
-    </div>
-  )
-}
+  return (
+    isLoaded && (
+      <div className="relative z-0 min-w-[18em] w-[18em] max-w-[18em] py-2 px-3 min-h-screen max-h-screen overflow-auto bg-midGray">
+        {serverClicked ? (
+          <Channels />
+        ) : (
+          <DirectMessagesList directMessages={directMessages} />
+        )}
+      </div>
+    )
+  );
+};
 
-export default MidSection
+export default MidSection;
