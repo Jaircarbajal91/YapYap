bc9055195b904f764e1d9d6d8fd74d9fb778f194
Jair Carbajal <jair.carbajal91@gmail.com>
2023-03-13 12:58:37 -0700

    added timezones and have user images working

--- frontend/src/components/Messages/index.jsx
+++ frontend/src/components/Messages/index.jsx
@@ -3,8 +3,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { sendMessage } from "../../store/messages";
 import { getDmMessages } from "../../store/messages";
 import { io } from "socket.io-client";
+import { zonedTimeToUtc, utcToZonedTime, format } from "date-fns-tz";
 
 const isProduction = process.env.NODE_ENV === "production";
+const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 export default function Messages({ messages, room }) {
   // const messages = Object.values(useSelector(state => state.messages));
@@ -44,21 +46,46 @@ export default function Messages({ messages, room }) {
 
   return (
     <div className="relative px-5 bg-chatBg max-w-full w-full min-h-screen max-h-screen overflow-auto">
-      <div className="p-3 flex flex-col w-full max-w-full">
+      <div className="p-3 flex flex-col w-full max-w-full max-h-[92%] overflow-auto">
         {messages.map((message) => {
           const { sender } = message;
-          console.log(sender);
+          console.log(message.image);
+          const newDate = Date.parse(message.updatedAt);
+          const formattedDate = format(
+            new Date(newDate),
+            "MM/dd/yyyy hh:mm aa",
+            { timeZone }
+          );
           return (
-            <div>
-              <p key={message.id} className="text-offWhite">
-                {message.message}
-              </p>
+            <div key={message.id} className="my-2 flex gap-4 items-center">
+              <div>
+                <img
+                  className="w-10 h-10 rounded-full"
+                  src={
+                    !message.image
+                      ? `https://api.dicebear.com/5.x/identicon/svg?seed=Aneka&backgroundType=gradientLinear`
+                      : message.image
+                  }
+                  alt="avatar"
+                />
+              </div>
+              <div>
+                <div className="flex gap-2 items-center">
+                  <span className="text-offWhite pb-1">
+                    {sender.alias || sender.username}
+                  </span>
+                  <span className="text-lightGray text-xs">
+                    {formattedDate}
+                  </span>
+                </div>
+                <p className="text-offWhite">{message.message}</p>
+              </div>
             </div>
           );
         })}
         {/* <p className="text-offWhite">{messageReceived}</p> */}
       </div>
-      <form className="absolute bottom-6 w-[95%] max-w-full" onSubmit={send}>
+      <form className="fixed bottom-6 w-[95%] max-w-full" onSubmit={send}>
         <input
           type="text"
           value={newMessage}
