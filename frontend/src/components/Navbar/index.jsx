9b592cba43140992721a5527bb7875b598bb682e
Kevin Zhang <zhang.kevin.hua@gmail.com>
2023-03-14 16:31:15 -0500

    added alt tags to navbar logo and space man

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
   return (
     <nav className="absolute flex top-0 justify-around w-full py-3">
       <div className="flex gap-1 cursor-pointer items-center navbar-brand" onClick={() => history.push('/')}>
-        <img className="w-[4em]" src={yapyap} alt="" />
-        <img className="w-[4em]" src={yapyapLogo} alt="" />
+        <img className="w-[4em]" src={yapyap} alt="space-man" />
+        <img className="w-[4em]" src={yapyapLogo} alt="yapyap-logo" />
       </div>
       <button onClick={() => history.push("/login")}
       className="bg-white tracking-wider text-black w-fit p-2 px-4 max-h-[3em] rounded-full hover:text-hero transition delay-75">Login</button>
