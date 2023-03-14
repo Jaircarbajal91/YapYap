44d02673b3d4ff5bab7f31e69e154bc58ef962d8
Jair Carbajal <jair.carbajal91@gmail.com>
2023-03-14 08:42:31 -0700

    fixed error causing messages to fail. Username was undefined because of alias

--- frontend/src/components/Messages/index.jsx
+++ frontend/src/components/Messages/index.jsx
@@ -48,7 +48,7 @@ export default function Messages({ messages, room }) {
     <div className="relative px-5 bg-chatBg max-w-full w-full min-h-screen max-h-screen overflow-auto">
       <div className="p-3 flex flex-col w-full max-w-full max-h-[92%] overflow-auto">
         {messages.map((message) => {
-          const { User: { username, alias } } = message;
+          const { sender: { username, alias } } = message;
           const newDate = Date.parse(message.updatedAt);
           const formattedDate = format(
             new Date(newDate),
