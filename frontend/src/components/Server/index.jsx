3b7ce6fafb29a641486edd547554c5bd24dabfb9
David Jetsupphasuk <jetsdavid4@gmail.com>
2023-03-15 14:44:11 -0700

    resolved merge conflict

--- frontend/src/components/Server/index.jsx
+++ frontend/src/components/Server/index.jsx
@@ -1,32 +1,3 @@
-<<<<<<< Updated upstream
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { getMessages } from "../../store/messages";
-import Channels from "../Channels";
-import ChannelModal from "../Channels/ChannelModal";
-import Messages from "../Messages";
-
-export default function ServerDetails({ sessionUser }) {
-  const { serverId } = useParams();
-  const dispatch = useDispatch();
-  const server = useSelector((state) => state.servers[serverId]);
-  const server_name = server?.server_name;
-  const channels = useSelector((state) => state.servers?.[serverId]?.Channels);
-  const test = useSelector((state) => state.channels);
-  console.log("use selector", test);
-  const messages = Object.values(useSelector((state) => state.messages));
-  const [channelId, setChannelId] = useState(null);
-  const [messagesLoaded, setMessagesLoaded] = useState([]);
-
-  async function selectChannel(e) {
-    e.preventDefault();
-    // display the messages of the channel that was clicked
-    setChannelId(e.target.id);
-    const messages = dispatch(getMessages(e.target.id));
-    setMessagesLoaded(messages);
-  }
-=======
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
@@ -57,7 +28,6 @@ export default function ServerDetails({ sessionUser }) {
         const messages = dispatch(getMessages(e.target.id));
         setMessagesLoaded(messages);
     }
->>>>>>> Stashed changes
 
   return (
     <div className="App flex relative w-full">
