983a2621f9e57839a5a6b90ebe79634e62e75178
Jair Carbajal <jair.carbajal91@gmail.com>
2023-03-15 13:30:55 -0700

    fixed styling for input in messages

--- frontend/src/components/Server/index.jsx
+++ frontend/src/components/Server/index.jsx
@@ -1,49 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { getMessages } from '../../store/messages';
-import Channels from '../Channels';
-import ChannelModal from '../Channels/ChannelModal';
-import Messages from '../Messages';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getMessages } from "../../store/messages";
+import Channels from "../Channels";
+import ChannelModal from "../Channels/ChannelModal";
+import Messages from "../Messages";
 
 export default function ServerDetails({ sessionUser }) {
-    const { serverId } = useParams();
-    const dispatch = useDispatch();
-    const server = useSelector(state => state.servers[serverId]);
-    const server_name = server?.server_name;
-    const channels = useSelector(state => state.servers?.[serverId]?.Channels);
-    const test = useSelector(state => state.channels)
-    console.log('use selector', test)
-    const messages = Object.values(useSelector(state => state.messages));
-    const [channelId, setChannelId] = useState(null);
-    const [messagesLoaded, setMessagesLoaded] = useState([]);
+  const { serverId } = useParams();
+  const dispatch = useDispatch();
+  const server = useSelector((state) => state.servers[serverId]);
+  const server_name = server?.server_name;
+  const channels = useSelector((state) => state.servers?.[serverId]?.Channels);
+  const test = useSelector((state) => state.channels);
+  console.log("use selector", test);
+  const messages = Object.values(useSelector((state) => state.messages));
+  const [channelId, setChannelId] = useState(null);
+  const [messagesLoaded, setMessagesLoaded] = useState([]);
 
-    async function selectChannel(e) {
-        e.preventDefault();
-        // display the messages of the channel that was clicked
-        setChannelId(e.target.id);
-        const messages = dispatch(getMessages(e.target.id));
-        setMessagesLoaded(messages);
-    }
+  async function selectChannel(e) {
+    e.preventDefault();
+    // display the messages of the channel that was clicked
+    setChannelId(e.target.id);
+    const messages = dispatch(getMessages(e.target.id));
+    setMessagesLoaded(messages);
+  }
 
-    return (
-        <div className='App flex relative w-full'>
-            <div className="relative scrollbar z-0 min-w-[18em] w-[18em] max-w-[18em] py-2 px-3 min-h-screen max-h-screen overflow-auto bg-midGray flex flex-col items-start">
-                <h1 className='w-full text-offWhite text-lg ml-1 mb-5'>{server_name}</h1>
-                <div className='flex w-full justify-between'>
-                    <h1 className='text-offWhite text-lg ml-1'>Text Channels</h1>
-                    <ChannelModal />
-                </div>
-                {channels && channels.map(channel => {
-                    return (
-                    <button key={channel.id} id={channel.id} className="text-lg" onClick={selectChannel}>
-                        # {channel.channel_name}
-                    </button>
-                )})}
-            </div>
-            <div className="relative scrollbar p-5 bg-chatBg w-full min-h-screen max-h-screen overflow-auto">
-                {messages.length > 0 && <Messages messages={messages} channelId={channelId}/>}
-            </div>
+  return (
+    <div className="App flex relative w-full">
+      <div className="relative scrollbar z-0 min-w-[18em] w-[18em] max-w-[18em] py-2 px-3 min-h-screen max-h-screen overflow-auto bg-midGray flex flex-col items-start">
+        <h1 className="w-full text-offWhite text-lg ml-1 mb-5">
+          {server_name}
+        </h1>
+        <div className="flex w-full justify-between">
+          <h1 className="text-offWhite text-lg ml-1">Text Channels</h1>
+          <ChannelModal />
         </div>
-    );
+        {channels &&
+          channels.map((channel) => {
+            return (
+              <button
+                key={channel.id}
+                id={channel.id}
+                className="text-lg"
+                onClick={selectChannel}
+              >
+                # {channel.channel_name}
+              </button>
+            );
+          })}
+      </div>
+      {messages.length > 0 && (
+        <Messages messages={messages} channelId={channelId} />
+      )}
+    </div>
+  );
 }
